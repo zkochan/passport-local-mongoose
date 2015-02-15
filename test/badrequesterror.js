d4f2bc08bc259503bd4e019543e02db519a2f694
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-02-16 00:52:56 +0200

    Error types returned along with the error messages.

--- test/badrequesterror.js
+++ test/badrequesterror.js
@@ -6,13 +6,14 @@ describe('BadRequestError', function() {
     var error = new BadRequestError();
 
     expect(error.stack).to.exist;
-    expect(error.name).to.equal('BadRequestError');
+    expect(error.name).to.equal('authorizationError');
   });
 
-  it('should construct a bad request error with message passed', function() {
-    var error = new BadRequestError('Test');
+  it('should construct a bad request error with name and message passed', function() {
+    var error = new BadRequestError('errorName', 'errorMessage');
 
-    expect(error.message).to.equal('Test');
+    expect(error.name).to.equal('errorName');
+    expect(error.message).to.equal('errorMessage');
   });
 
   it('should construct a bad request error with null message if no message was passed', function() {
